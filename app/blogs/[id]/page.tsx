cdd1229f39b3590387ec440acfabefe82e8a2c7b
hazelfoxx <hazelfox008@gmail.com>
2025-01-13 13:01:55 +0530

    blog type id

--- app/blogs/[id]/page.tsx
+++ app/blogs/[id]/page.tsx
@@ -1,4 +1,3 @@
-// [id]/page.tsx
 import { BackButton } from "@/components/blog/BackButton";
 import { BlogContent } from "@/components/blog/BlogContent";
 import Newsletter from "@/components/home/newsletter";
@@ -6,13 +5,20 @@ import blogData from '../../../components/blog/blog-posts.json';
 import { notFound } from "next/navigation";
 import { Metadata } from "next";
 
+// Define params type for generateMetadata
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
 export async function generateStaticParams() {
   return blogData.posts.map((post) => ({
     id: post.id.toString(),
   }));
 }
 
-export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const post = blogData.posts.find(post => post.id.toString() === params.id);
 
   if (!post) {
@@ -25,7 +31,7 @@ export async function generateMetadata({ params }: { params: { id: string } }): 
   };
 }
 
-export default function BlogPost({ params }: { params: { id: string } }) {
+export default function BlogPost({ params }: Props) {
   const post = blogData.posts.find(post => post.id.toString() === params.id);
 
   if (!post) {
@@ -33,14 +39,10 @@ export default function BlogPost({ params }: { params: { id: string } }) {
   }
 
   return (
-    <main className="min-h-screen bg-background">
-      <div className="max-w-7xl mx-auto py-4">
-        <BackButton />
-        <BlogContent post={post} />
-      </div>
-      <div className="py-4">
-        <Newsletter />
-      </div>
-    </main>
+    <div className="container mx-auto px-4 py-8">
+      <BackButton />
+      <BlogContent post={post} />
+      <Newsletter />
+    </div>
   );
 }
